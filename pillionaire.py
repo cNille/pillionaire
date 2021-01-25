725efef3ca437c64e99eaca7f5628937ac574b8c
Christopher Nilsson <christopher@sanalabs.com>
2021-01-25 11:18:05 +0100

    test sanians

--- pillionaire.py
+++ pillionaire.py
@@ -38,10 +38,10 @@ def ascii_to_letters(numbers):
 def letter_to_ascii(word):
     return "".join([
         str(ord(c) - 96)
-        for c in word.lower()
+        for c in word.lower().replace(' ', '')
     ])
 
-def pillionaire(word):
+def pillionaire(word, stop_at_first):
     search = letter_to_ascii(word)
 
     last_char = ''
@@ -52,7 +52,7 @@ def pillionaire(word):
             count = 0
             for piece in read_in_chunks(f):
                 count += 1
-                if count % 100000 == 0:
+                if count % 100000 == 0 and not stop_at_first:
                     msg = "Done... %d %%" % (count / 10000)
                     sys.stdout.write(u"\u001b[20D" + msg)
                     sys.stdout.flush()
@@ -60,9 +60,9 @@ def pillionaire(word):
                 if search in piece:
                     idx = piece.index(search)
                     position = idx + count *1024
-                    before =  piece[idx-4:idx]
+                    before =  piece[idx-2:idx]
                     found = piece[idx:idx+len(search)]
-                    after = piece[idx+len(search):idx+len(search)+4] 
+                    after = piece[idx+len(search):idx+len(search)+2] 
                     numbers = white + before + green + found + white + after
                     b = white + ascii_to_letters(before)
                     f = green + word 
@@ -70,11 +70,16 @@ def pillionaire(word):
                     txt = b+f+a 
 
                     results.append("Position %d: %s" % (position, numbers + ' -> ' + txt))
+                    if stop_at_first:
+                        pos_str = "{:,}".format(position)
+                        return position, "At position %s we find %s: %s" % (pos_str, word, numbers + ' -> ' + txt)
                 last_char = piece[-1]
 
                 if len(results) > 20:
                     break
 
+            if stop_at_first:
+                return
             sys.stdout.write(u"\u001b[20D" + "Done... 100%")
             sys.stdout.flush()
             print ""
@@ -88,4 +93,56 @@ def pillionaire(word):
         print "ERROR:"
         print "./pi-billion.txt is missing. Download from: %s" % "https://stuff.mit.edu/afs/sipb/contrib/pi/"
 
-pillionaire(sys.argv[-1])
+if len(sys.argv) == 2:
+    pillionaire(sys.argv[-1], False)
+
+else:
+    print "Who is most PI in sana?"
+
+    sanians = [
+        "alex",
+        "anna",
+        "chris",
+        "damjan",
+        "elyes",
+        "emil",
+        "faruk",
+        "fredde",
+        "fredrik",
+        "jaromir",
+        "jasmine",
+        "jenny",
+        "jesper",
+        "joel",
+        "jon",
+        "ludvig",
+        "lundgren",
+        "mai",
+        "miruna",
+        "nille",
+        "oscar",
+        "patrik",
+        "samuel",
+        "seb",
+        "sebastian",
+        "sofie",
+        "susanna",
+        "ulf",
+        "vale",
+        "victor",
+        "viktor",
+            ]
+
+
+    results = [(s,pillionaire(s, True)) for s in sanians]
+    no_match = [r[0] for r in results if not r[1]]
+    results = [r[1] for r in results if r[1]]
+
+    results.sort(key=lambda x: x[0])
+    for r in results:
+        print r[1]
+
+    print "Not within a billion digits..."
+    for r in no_match:
+        print r
+
